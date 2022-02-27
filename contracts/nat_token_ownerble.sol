56b93cbfaf73441c414dab076e00115b1b4acf1f
Ad450 <adjeimanue@gmail.com>
2022-02-27 17:52:29 +0000

    switch ownership added, refactor needed

---
+++ contracts/nat_token_ownerble.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+/// @title NAT TOKEN   
+/// @author Emmanuel
+/// @notice an interface that guides the the NAT token
+/// @dev A token standard based on the ERC20 standards
+
+contract Ownerable {
+    address public owner;
+
+    address public newOwner;
+
+    function transferOwnership (address  _newOwner) public {
+        require (msg.sender == owner, "you are authorized to perform this operation");
+        
+    }
+}
