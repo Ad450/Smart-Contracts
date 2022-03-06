3632c94cae0a262d3c69ac2c757f6523fd463c25
Ad450 <adjeimanue@gmail.com>
2022-03-06 14:28:41 +0000

    burnable added

---
+++ contracts/nat_token_burnable.sol
@@ -0,0 +1,24 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+/// @title NAT TOKEN BURNABLE 
+/// @author Emmanuel
+/// @notice an interface that guides the the NAT token
+/// @dev A token standard based on the ERC20 standards
+
+import "./nat_token.sol";
+
+contract NatTokenBurnable is NatToken{
+    address private constant BURN = 0x000000000000000000000000000000000000dEaD;
+    
+     /*  
+  @notice burn destroys some _amount of tokens in circulation
+  @notice we transfer _amount of tokens to the eater address (0x0...)
+  @notice this locks the tokens forever because this address has no private key and it is not owned
+  @param _amount is the amount of tokens to be destroyed
+
+   */
+
+    function burn(uint _amount) public {
+        transfer(BURN, _amount);
+    }
+}
