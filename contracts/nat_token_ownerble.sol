8b8f89532b80532aee24ceae9f04f13fb4370ad6
Ad450 <adjeimanue@gmail.com>
2022-03-10 23:16:19 +0000

    restored all missing overrides

--- contracts/nat_token_ownerble.sol
+++ contracts/nat_token_ownerble.sol
@@ -5,13 +5,14 @@ pragma solidity ^0.8.4;
 /// @notice an interface that guides the the NAT token
 /// @dev A token standard based on the ERC20 standards
 
-contract Ownerble {
-    address public owner;
+import "./nat_token.sol";
+
+contract NatTokenOwnerble is NatToken {
+    address public override owner;
 
     address public newOwner;
 
     function transferOwnership (address  _newOwner) public {
-        require (msg.sender == owner, "you are authorized to perform this operation");
-        
+        require (msg.sender == owner, "you are authorized to perform this operation");    
     }
 }
