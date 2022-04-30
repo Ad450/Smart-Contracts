0cc66d8fe43d80dfe892f247ae0f9d960ebbb69f
Ad450 <adjeimanue@gmail.com>
2022-04-30 23:38:31 +0000

    add product function

--- contracts/Product-Identification/product_identification_interface.sol
+++ contracts/Product-Identification/product_identification_interface.sol
@@ -1,7 +1,15 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+/// @author Emmanuel
+/// @title Product Identification System
+/// @dev all functions are still under development
 contract ProductIndentification {
+    /// @notice a product object, all products will have these properties
+    /// @param `sender` is address of the manufacturing companies
+    /// @param `name` is the name of the product
+    /// @param `code` a hash of the product id
+    /// @param `id` is a unique id of the product
     struct Product {
         address sender;
         string name;
@@ -10,5 +18,12 @@ contract ProductIndentification {
         uint256 date;
     }
 
+    /// @notice keeping tracks of companies and their products
+    /// @dev this mapping can be advanced with address => Product[]
     mapping(address => Product) private companyProducts;
+
+    /// @notice companies will add products with this method
+    /// @param _product is the product to be added
+    /// @dev function still under development
+    function addProduct(Product memory _product) public {}
 }
