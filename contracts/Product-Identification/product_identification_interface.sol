f94a4f5d27bbdc34d1fb6005c4c2399efd99b94c
Ad450 <adjeimanue@gmail.com>
2022-05-01 16:14:03 +0000

    more functionalities added

--- contracts/Product-Identification/product_identification_interface.sol
+++ contracts/Product-Identification/product_identification_interface.sol
@@ -5,11 +5,18 @@ pragma solidity ^0.8.4;
 /// @title Product Identification System
 /// @dev all functions are still under development
 contract ProductIndentification {
+    ///@notice isAdded to check if product has already been added
+    bool private isAdded = false;
+
+    /// @notice productAdded event will be fired when any product is added
+    event ProductAdded();
+
     /// @notice a product object, all products will have these properties
-    /// @param `sender` is address of the manufacturing companies
-    /// @param `name` is the name of the product
-    /// @param `code` a hash of the product id
-    /// @param `id` is a unique id of the product
+    /// @param sender is address of the manufacturing companies
+    /// @param name is the name of the product
+    /// @param code a hash of the product id
+    /// @param id is a unique id of the product
+    /// @param date
     struct Product {
         address sender;
         string name;
@@ -20,12 +27,62 @@ contract ProductIndentification {
 
     /// @notice keeping tracks of companies and their products
     /// @dev this mapping can be advanced with address => Product[]
-    mapping(address => Product[]) private productStore;
+    mapping(address => bytes32[]) private productStore;
+
+    /// @notice keeps track manufacturers and their public addresses
+    mapping(string => address) private manufacturers;
+
+    /// @notice onlyManufacturer requires manufacturer to be registered
+    /// @param _manufacturer is the name of the manufacturer
+    /// @param _manufacturerAddress is the address of manufacturer
+    modifier onylManufacturer(
+        string memory _manufacturer,
+        address _manufacturerAddress
+    ) {
+        require(
+            manufacturers[_manufacturer] == _manufacturerAddress,
+            "manufacturer not found"
+        );
+        _;
+    }
 
     /// @notice companies will add products with this method
-    /// @param _product is the product to be added
+    /// @param _manufacturer is the name of the manufacturer
+    /// @param _code is a unique string found on the product
+    /// @param id is a unique nonce added to the product submission form
+    /// @param _manufacturerAddress address of the manufacturer
     /// @dev function still under development
-    function addProduct(Product memory _product) public {}
+    function addProduct(
+        uint256 id,
+        uint256 date,
+        address _manufacturerAddress,
+        string memory _code,
+        string memory _manufacturer
+    ) public onylManufacturer(_manufacturer, _manufacturerAddress) {
+        Product memory _product = Product(
+            _manufacturerAddress,
+            _manufacturer,
+            _code,
+            id,
+            date
+        );
+
+        require(!isAdded, "product already added");
+        uint256 arrayLength = productStore[_manufacturerAddress].length;
+
+        bytes32 _productHash = _computeHash(_product.code);
+        for (uint256 i = 0; i < arrayLength; i++) {
+            if (!isAdded) {
+                if (_productHash == productStore[_manufacturerAddress][i]) {
+                    productStore[_manufacturerAddress].push(_productHash);
+                    isAdded = true;
+                    emit ProductAdded();
+                }
+            } else {
+                revert("product already added");
+            }
+        }
+    }
 
     /// @notice computeHash produces the hash of the id of the product
     /// @param _productCode is the code of the product to be hashed
@@ -48,4 +105,14 @@ contract ProductIndentification {
         view
         returns (bool)
     {}
+
+    /// @notice register manufacturers by under their names
+    /// @param _manufacturer is the the name of the manufacturer
+    /// @param _manufacturerAddress is the address of the manufacturer
+    function registerManufacturer(
+        string memory _manufacturer,
+        address _manufacturerAddress
+    ) public {
+        manufacturers[_manufacturer] = _manufacturerAddress;
+    }
 }
