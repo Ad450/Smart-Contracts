8d872726d206f7af38246f265654ef541c933336
Ad450 <adjeimanue@gmail.com>
2022-05-03 00:48:54 +0000

    add product test failing

--- test/product_identification_test.ts
+++ test/product_identification_test.ts
@@ -10,12 +10,55 @@ import productContractJson from "../artifacts/contracts/Product-Identification/p
 describe("product identication contract test", () => {
   let contractInstance: Contract;
   let wallet;
+  let testAddress1: any;
+  const testManufacturer: String = "Geocem";
+  const productCode: String = "123456";
+
   beforeEach(async () => {
     wallet = await new MockProvider().getWallets();
     contractInstance = await deployContract(wallet[0], productContractJson);
+    testAddress1 = wallet[1].address;
   });
 
   it("should deploy contract ", async () => {
     console.log(contractInstance.address);
   });
+
+  it("should register a manufacturer", async () => {
+    // ARRANGE
+    await contractInstance.registerManufacturer(testManufacturer, testAddress1);
+
+    // ACT
+    const result = await contractInstance.getManufacturers(testManufacturer);
+
+    // ASSERT
+    expect(result).equals(testAddress1);
+  });
+
+  it("add products to a manufacturers array", async () => {
+    // ARRANGE
+    await contractInstance.registerManufacturer(testManufacturer, testAddress1);
+
+    // ACT
+    await contractInstance.addProduct(
+      1,
+      2,
+      testAddress1,
+      productCode,
+      testManufacturer
+    );
+
+    const result = await contractInstance.searchProduct(
+      productCode,
+      testAddress1
+    );
+    // ASSERT
+    expect(result).equals(true);
+  });
 });
+
+// uint256 id,
+// uint256 date,
+// address _manufacturerAddress,
+// string memory _code,
+// string memory _manufacturer
