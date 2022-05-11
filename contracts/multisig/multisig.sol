66d7fe70e1ffb56532e89c1a9f05568acfd3d61e
Ad450 <adjeimanue@gmail.com>
2022-05-11 03:01:43 -0700

    initial commit

---
+++ contracts/multisig/multisig.sol
@@ -0,0 +1,14 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.4;
+
+
+/// @author Emmanuel
+/// @title Multisig
+/// @dev all contract functions are still under development
+contract Multisig{
+
+    /// @notice keeps track of all owners 
+    address[] public owners;
+    
+    
+}
