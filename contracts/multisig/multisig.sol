4b3282e44c1b41c7915644159f1c41027cf6a94d
Ad450 <adjeimanue@gmail.com>
2022-05-12 14:02:38 -0700

    new implementation

--- contracts/multisig/multisig.sol
+++ contracts/multisig/multisig.sol
@@ -6,9 +6,54 @@ pragma solidity ^0.8.4;
 /// @title Multisig
 /// @dev all contract functions are still under development
 contract Multisig{
-
-    /// @notice keeps track of all owners 
+    // owners of contract
     address[] public owners;
+
+    // required number of owners to validate/ approve a transaction
+    uint public required; 
     
-    
+    // keep track of owners
+    mapping (address => bool) isOwner;
+
+    // an array of transactions
+    Transaction[] private transactions;
+
+    // events 
+    event Approved(uint txId);
+    event Revoked(uint txId);
+    event Executed(uint txId);
+    event TransactionCreated(uint txId);
+
+
+    // init owners at deployment
+    constructor (address[] memory _owners, uint _required){
+        require(_owners.length > 0, "invalid owner array");
+        require(required > 0, "invalid required value");
+
+        for(uint i = 0; i < _owners.length; i++){
+            owners.push(_owners[i]);
+
+            isOwner[_owners[i]] = true;
+        }
+
+        required = _required;
+    }    
+
+    // transaction struct
+
+    struct Transaction{
+        uint txId;
+        bool isApproved;
+        uint approvals;
+        address to;
+        uint amount;
+    }
+
+
+    // create transaction
+    function createTransaction(address _to, uint _txId, uint _amount) public {
+        require(isOwner[msg.sender], "not authorised");
+        emit TransactionCreated(_txId);
+        transactions.push(Transaction(_txId, false, 1, _to, _amount));
+    }
 }
