821bf4cd85144b506dd0bcf528d3cf2b3056d59f
Ad450 <adjeimanue@gmail.com>
2022-05-23 22:35:31 -0700

    multisig deployment passing

--- test/multisig_test.ts
+++ test/multisig_test.ts
@@ -4,23 +4,25 @@ import { expect } from "chai";
 /* eslint-disable node/no-missing-import */
 import { ethers } from "hardhat";
 import { deployContract, MockProvider } from "ethereum-waffle";
-import { Contract } from "ethers";
+import { BigNumber, Contract } from "ethers";
 import multisigJson from "../artifacts/contracts/multisig/multisig.sol/MultisigWallet.json";
 import { beforeEach } from "mocha";
 
 describe("Multisig Contract", async () => {
     let multisig : Contract;
     let wallet;
-    
+    let owners: Array<string>;
 
+    
+    
     beforeEach(async ()=>{
         wallet = await new MockProvider().getWallets();
-        let owners : Array<string> = [wallet[0].address, wallet[1].address , wallet[2].address,];
-        multisig = await deployContract(wallet[0], multisigJson, [owners, 4]);
+        owners = [wallet[0].address, wallet[1].address , wallet[2].address,];
+        multisig = await deployContract(wallet[0], multisigJson, [  owners ,3]);
+       
     })
 
     it("should deploy contract", async ()=>{
-       console.log(multisig.address);
-        
+       console.log(multisig.address);      
     });
 });
