d223e735ef1bceca0aa2815cb419efef80d54c90
Ad450 <adjeimanue@gmail.com>
2022-06-07 15:47:25 +0100

    dexchange token (LP fee)

---
+++ contracts/proof-of-concepts/dexchange_ERC20.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unilicense
+pragma solidity ^0.8.4;
+import "../ERC20/nat_token.sol";
+
+contract DexchangeERC20 is NatToken {
+    // function mint(uint256 _amount) internal view override {
+    //     uint256 _supply = totalSupply();
+    //     _supply += _amount;
+    // }
+}
