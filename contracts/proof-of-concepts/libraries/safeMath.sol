1ade48306799e235d771629c81382232f219de79
Ad450 <adjeimanue@gmail.com>
2022-06-08 17:52:07 +0100

    safe guard maths operations

---
+++ contracts/proof-of-concepts/libraries/safeMath.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: Unilicense
+pragma solidity ^0.8.4;
+
+library SafeMath {
+    // safe multiplication
+    function mul(uint256 a, uint256 b) public returns (uint256 result) {
+        result = (a * b);
+        require((result / a) == b, "incorrect multiplication");
+
+        return result;
+    }
+
+    // safe addition
+    function add(uint256 a, uint256 b) public returns (uint256 result) {
+        result = (a + b);
+        require(result >= (a + b), "incorrect addition");
+
+        return result;
+    }
+
+    // safe division
+    function div(uint256 a, uint256 b) public returns (uint256 result) {
+        // where a is always the numerator
+        require(b > 0, "indeterminate");
+        result = (a / b);
+        require((result * b) == a, "incorrect division");
+
+        return result;
+    }
+
+    // safe subtraction
+    function sub(uint256 a, uintb) public returns (uint256 result) {
+        require(a > b, "negative result ");
+        result = (a - b);
+        require(result < a, "incorrect subtraction");
+
+        return result;
+    }
+}
